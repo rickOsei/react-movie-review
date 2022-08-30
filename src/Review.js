2510d191b35e32427d0d5eba72875aadc50b1635
Osei Rick <rickoseiampah@gmail.com>
2022-08-30 16:32:38 +0300

    First commit

--- src/Review.js
+++ src/Review.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
 
 const key = "9WouZZwa9H2ftdsaUazlM23fIRtgrBVr";
 const url = `https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=${key}`;
@@ -8,6 +9,7 @@ export default class Review extends Component {
     super(props);
     this.state = {
       movies: [],
+      Loading: true,
     };
   }
 
@@ -15,11 +17,56 @@ export default class Review extends Component {
     fetch(url)
       .then((response) => response.json())
       .then((reviews) => {
-        console.log(reviews);
+        this.setState({ Loading: false });
+        this.setState({ movies: reviews.results });
       })
       .catch((error) => console.log(error));
   }
+
   render() {
-    return <div>Review</div>;
+    return (
+      <>
+        <main className="container">
+          <div className="m-title">
+            <h2 className="heading">movies review</h2>
+            <div className="underline"></div>
+          </div>
+          <article className="menu">
+            {this.state.movies.map((review, index) => {
+              return <Movies key={index} {...review} />;
+            })}
+          </article>
+        </main>
+      </>
+    );
   }
 }
+
+const Movies = ({
+  display_title,
+  mpaa_rating,
+  critics_pick,
+  byline,
+  headline,
+  summary_short,
+  publication_date,
+}) => {
+  return (
+    <div className="movie">
+      <div className="title">
+        <h2>{display_title}</h2>
+      </div>
+      <div className="details">
+        <h4 className="rating">{mpaa_rating || "Unrated"}</h4>
+        <h4 className="genre">{critics_pick}</h4>
+        <h4 className="direct">{publication_date}</h4>
+      </div>
+      <p className="info">{summary_short}</p>
+      <h4 className="byline">
+        <span>by</span> {byline}
+      </h4>
+
+      <h4 className="headline">{headline}</h4>
+    </div>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    display_title: "The Invitation",
+    mpaa_rating: "PG-13",
+    critics_pick: 0,
+    byline: "Natalia Winkelman",
+    headline: "‘The Invitation’ Review: Bringing Down the Haunted House",
+    summary_short:
+      "Nathalie Emmanuel stars as the unwitting belle of an English manor in this middling gothic horror movie that leaves her blind to the blood-red flags waving at every turn.",
+    publication_date: "2022-08-26",
+    opening_date: "2022-08-26",
+    date_updated: "2022-08-26 18:40:06",
+    link: {
+      type: "article",
+      url: "https://www.nytimes.com/2022/08/26/movies/the-invitation-review.html",
+      suggested_link_text: "Read the New York Times Review of The Invitation",
+    },
+  },
+];
